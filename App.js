e889b5e1136b83fcea9a2799884079243c5918f0
vladchornii <vladyslav.chornii.kb.2022@lpnu.ua>
2023-10-25 21:27:58 +0300

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! Heeeeey</Text>
+      <Text>Hello World! Hui</Text>
       <StatusBar style="auto" />
     </View>
   );
