d47e080a39d9716d37a557dc58ef56ec8994e88c
Iryna Pajonchkivska <irynapajonchkivska@gmail.com>
2023-11-15 01:56:38 +0200

    Updates

--- src/smallRoom.js
+++ src/smallRoom.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity} from "react-native";
 import { globalStyle } from "../styles/style";
-import { moreDetailedChoiceStyles } from '../styles/moreDetailedChoiceStyles';
 
 export const SmallRoom = ({ navigation }) =>{
   return (
  <View style={globalStyle.main}>
-    <TouchableOpacity style={moreDetailedChoiceStyles.arrow} onPress={() => {navigation.navigate("YourChoicePremisesLandscape"); }}>
+    <TouchableOpacity onPress={() => {navigation.navigate("YourChoicePremisesLandscape"); }}>
       <Image source={require("../assets/images/arrow.png")}/>
     </TouchableOpacity>
-    <Image style={moreDetailedChoiceStyles.image} source={require("../assets/images/moreDetailedChoicePrLaIcon.png")}/>
-    <Text style={[globalStyle.text, moreDetailedChoiceStyles.headerText]}>Ви обрали:</Text>
-    <Text style={[globalStyle.text, moreDetailedChoiceStyles.buttonText]}>Невелике приміщення</Text>
+    <Image source={require("../assets/images/moreDetailedChoicePrLaIcon.png")}/>
+    <Text>Ви обрали:</Text>
+    <TouchableOpacity>
+      <Text style={globalStyle.text}>Невелике приміщення</Text>
+    </TouchableOpacity>
     <Image source={require("../assets/images/downPointer.png")}/>
-    <TouchableOpacity style={moreDetailedChoiceStyles.arrow} onPress={() => {navigation.navigate("Walls"); }}>
+    <TouchableOpacity onPress={() => {navigation.navigate("Walls"); }}>
     <Text style={[globalStyle.text]}>Стіни</Text>
+    </TouchableOpacity>
+    <TouchableOpacity >
+    <Text style={[globalStyle.text]}>Підлога</Text>
+    </TouchableOpacity>
+    <TouchableOpacity >
+    <Text style={[globalStyle.text]}>Стеля</Text>
     </TouchableOpacity>
  </View>
   );
