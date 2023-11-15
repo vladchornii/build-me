4edeff9b4d7a9de284e681129e0d0d8cd9b15848
Ksiushka3 <sapsajoksana05@gmail.com>
2023-11-15 11:23:33 +0200

    add authorization

--- src/authorization.js
+++ src/authorization.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { View, Text, Image, TextInput, TouchableOpacity, KeyboardAvoidingView} from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Alert} from "react-native";
 import { globalStyle } from "../styles/style";
 import { authorizationStyles } from "../styles/authorizationStyles";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from "./firebase-config";
 
 export const Authorization = ({ navigation }) => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
 
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password )
+    .then((userCredential) => {
+      console.log('Signed In!')
+      const user = userCredential.user ;
+      console.log(user)
+      navigation.navigate("Subscription");
+      Alert.alert('You signed in!')
+
+
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
   return (
     <KeyboardAvoidingView behavior="padding" style={[globalStyle.main, authorizationStyles.box]}>
       <View style={[globalStyle.main, authorizationStyles.box]}>
@@ -28,7 +49,12 @@ export const Authorization = ({ navigation }) => {
           placeholderTextColor='rgba(244, 240, 232, 1)'
           secureTextEntry={true}
         />
-        <TouchableOpacity style={authorizationStyles.continueButton} onPress={() => {navigation.navigate("Subscription"); }}>
+        <TouchableOpacity 
+        style={authorizationStyles.continueButton} 
+        onPress={() => {
+          handleSignIn();
+          // navigation.navigate("Subscription");
+        }}>
           <Text style={authorizationStyles.continueButtonText}>Продовжити</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => {navigation.navigate("Registration"); }}>
